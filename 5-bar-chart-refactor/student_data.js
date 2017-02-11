4406eabab057b07ee332248a91389a4350ea77c1
Loren Stewart <l.s.stewart111@gmail.com>
2017-02-11 11:13:53 -0800

    added a lot, okay?

---
+++ 5-bar-chart-refactor/student_data.js
@@ -0,0 +1,42 @@
+export const studentData = [
+        {
+          student_id: 101,
+          name: 'Gary Glendale',
+          score: 83
+        },
+        {
+          student_id: 102,
+          name: 'Paul Perry',
+          score: 42
+        },
+        {
+          student_id: 103,
+          name: 'Jameel Jennings',
+          score: 96
+        },
+        {
+          student_id: 104,
+          name: 'Becky Barkson',
+          score: 55
+        },
+        {
+          student_id: 105,
+          name: 'Jimmy Jimenez',
+          score: 70
+        },
+        {
+          student_id: 106,
+          name: 'Kali Fornia',
+          score: 60
+        },
+        {
+          student_id: 107,
+          name: 'Bob Burlap',
+          score: 98
+        },
+        {
+          student_id: 108,
+          name: 'Sharice Simmons',
+          score: 81
+        }
+      ];
---
+++ 5-bar-chart-refactor/student_data_2.js
@@ -0,0 +1,42 @@
+export const studentData2 = [
+        {
+          student_id: 101,
+          name: 'Gary Glendale',
+          score: 43
+        },
+        {
+          student_id: 102,
+          name: 'Paul Perry',
+          score: 97
+        },
+        {
+          student_id: 103,
+          name: 'Jameel Jennings',
+          score: 37
+        },
+        {
+          student_id: 104,
+          name: 'Becky Barkson',
+          score: 78
+        },
+        {
+          student_id: 105,
+          name: 'Jimmy Jimenez',
+          score: 53
+        },
+        {
+          student_id: 106,
+          name: 'Kali Fornia',
+          score: 91
+        },
+        {
+          student_id: 107,
+          name: 'Bob Burlap',
+          score: 71
+        },
+        {
+          student_id: 108,
+          name: 'Sharice Simmons',
+          score: 60
+        }
+      ];
